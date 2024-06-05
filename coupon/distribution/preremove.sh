672050f19333372a4775d04c380d0ac2d09c032c
Rauno Viskus <rauno56@gmail.com>
2024-06-05 12:51:53 +0300

    feat: build package for pricing service

--- coupon/distribution/preremove.sh
+++ coupon/distribution/preremove.sh
@@ -1,4 +1,4 @@
 if command -v systemctl >/dev/null 2>&1; then
-    systemctl stop odigos-demo-inventory.service
-    systemctl disable odigos-demo-inventory.service
+    systemctl stop odigos-demo-coupon.service
+    systemctl disable odigos-demo-coupon.service
 fi
