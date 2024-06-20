9f3e09c9a291800b3e9e326f473e6bae231bc317
Rauno Viskus <rauno56@gmail.com>
2024-06-20 11:13:58 +0300

    fix: use the same env var for host references for every service

--- coupon/app.js
+++ coupon/app.js
@@ -1,8 +1,21 @@
+const assert = require('assert/strict');
+const util = require('util');
+
 const express = require('express');
 
 const PORT = parseInt(process.env.PORT || '8080');
 const app = express();
 const axios = require('axios');
+
+const {
+  MEMBERSHIP_SERVICE_HOST,
+} = process.env;
+
+assert.equal(
+  typeof MEMBERSHIP_SERVICE_HOST,
+  'string',
+  `Expexted 'MEMBERSHIP_SERVICE_HOST' to be set. Got: ${util.format(MEMBERSHIP_SERVICE_HOST)}`
+);
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -16,7 +29,7 @@ app.get('/coupons', (req, res) => {
   console.log('Coupon request received!');
 
   // Fetch from membership api
-  axios.get(`http://${process.env.MEMBERSHIP_SERVICE_URL}/isMember`)
+  axios.get(`http://${MEMBERSHIP_SERVICE_HOST}/isMember`)
     .then(function (response) {
       console.log('Got response from membership service!', response.data);
       res.json({
@@ -33,7 +46,7 @@ app.post('/apply-coupon', (req, res) => {
   console.log('Applying coupon!');
 
     // Fetch from membership api
-    axios.get(`http://${process.env.MEMBERSHIP_SERVICE_URL}/isMember`)
+    axios.get(`http://${MEMBERSHIP_SERVICE_HOST}/isMember`)
     .then(function (response) {
       console.log('Got response from membership service!', response.data);
       res.json({
