682e046eeb69ee3e628e1bd03ce10f25112d7810
Amir Blum <amirgiraffe@gmail.com>
2024-07-21 11:15:47 +0300

    feat: add health checks and manual spans to demo

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ generate-webapp:
 .PHONY: build-images
 build-images:
 	@echo "Building images..."
-	docker build -t dev/odigos-demo-frontend:dev $(PROJECT_DIR)frontend -f $(PROJECT_DIR)frontend/Dockerfile
+	docker build -t dev/frontend:dev $(PROJECT_DIR)frontend -f $(PROJECT_DIR)frontend/Dockerfile
 	docker build -t dev/inventory:dev $(PROJECT_DIR)inventory -f $(PROJECT_DIR)inventory/Dockerfile
 	docker build -t dev/pricing:dev $(PROJECT_DIR)pricing -f $(PROJECT_DIR)pricing/Dockerfile
 	docker build -t dev/coupon:dev $(PROJECT_DIR)coupon -f $(PROJECT_DIR)coupon/Dockerfile
@@ -20,7 +20,7 @@ build-images:
 .PHONY: load-to-kind
 load-to-kind:
 	@echo "Loading images to kind..."
-	kind load docker-image dev/odigos-demo-frontend:dev
+	kind load docker-image dev/frontend:dev
 	kind load docker-image dev/inventory:dev
 	kind load docker-image dev/pricing:dev
 	kind load docker-image dev/coupon:dev
@@ -43,3 +43,13 @@ build-push-images-prod:
 	docker buildx build -t keyval/odigos-demo-pricing:${VERSION} $(PROJECT_DIR)pricing -f $(PROJECT_DIR)pricing/Dockerfile --platform linux/amd64,linux/arm64 --push
 	docker buildx build -t keyval/odigos-demo-coupon:${VERSION} $(PROJECT_DIR)coupon -f $(PROJECT_DIR)coupon/Dockerfile --platform linux/amd64,linux/arm64 --push
 	docker buildx build -t keyval/odigos-demo-membership:${VERSION} $(PROJECT_DIR)membership -f $(PROJECT_DIR)membership/Dockerfile --platform linux/amd64,linux/arm64 --push
+
+
+.PHONY: deploy-membership
+deploy-membership: 
+	@echo "Deploying membership to Kubernetes..."
+	docker build -t dev/membership:dev $(PROJECT_DIR)membership -f $(PROJECT_DIR)membership/Dockerfile
+	kind load docker-image dev/membership:dev
+	kubectl apply -f $(PROJECT_DIR)membership/deployment/
+	kubectl rollout restart deployment membership
+	
--- coupon/app.js
+++ coupon/app.js
@@ -1,40 +1,54 @@
-const assert = require('assert');
-const util = require('util');
+const assert = require("assert");
+const util = require("util");
+const { trace } = require("@opentelemetry/api");
 
-const express = require('express');
+const express = require("express");
 
-const PORT = parseInt(process.env.PORT || '8080');
+const PORT = parseInt(process.env.PORT || "8080");
 const app = express();
-const axios = require('axios');
+const axios = require("axios");
 
-const {
-  MEMBERSHIP_SERVICE_HOST,
-} = process.env;
+const { MEMBERSHIP_SERVICE_HOST } = process.env;
 
 assert.equal(
   typeof MEMBERSHIP_SERVICE_HOST,
-  'string',
-  `Expexted 'MEMBERSHIP_SERVICE_HOST' to be set. Got: ${util.format(MEMBERSHIP_SERVICE_HOST)}`
+  "string",
+  `Expexted 'MEMBERSHIP_SERVICE_HOST' to be set. Got: ${util.format(
+    MEMBERSHIP_SERVICE_HOST
+  )}`
 );
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-app.get('/test', (req, res) => {
-  res.send('Coupon service is up and running!');
+app.get("/healthz", (req, res) => {
+  res.status(200).send("Coupon service is up and running!");
 });
 
-app.get('/coupons', (req, res) => {
-  console.log('Coupon request received!');
+app.get("/coupons", (req, res) => {
+  console.log("Coupon request received!");
+
+  const appTracer = trace.getTracer("coupon");
+  const newCouponNumber = appTracer.startActiveSpan(
+    "generate new coupon",
+    (span) => {
+      try {
+        return getRandomNumber(1, 25);
+      } finally {
+        span.end();
+      }
+    }
+  );
 
   // Fetch from membership api
-  axios.get(`http://${MEMBERSHIP_SERVICE_HOST}/isMember`)
+  axios
+    .get(`http://${MEMBERSHIP_SERVICE_HOST}/isMember`)
     .then(function (response) {
-      console.log('Got response from membership service!', response.data);
+      console.log("Got response from membership service!", response.data);
       res.json({
-        coupon: getRandomNumber(1, 25),
-        isMember: response.data.isMember
+        coupon: newCouponNumber,
+        isMember: response.data.isMember,
       });
     })
     .catch(function (error) {
@@ -42,16 +56,29 @@ app.get('/coupons', (req, res) => {
     });
 });
 
-app.post('/apply-coupon', (req, res) => {
-  console.log('Applying coupon!');
+app.post("/apply-coupon", (req, res) => {
+  console.log("Applying coupon!");
 
-    // Fetch from membership api
-    axios.get(`http://${MEMBERSHIP_SERVICE_HOST}/isMember`)
+  const appTracer = trace.getTracer("coupon");
+  const newCouponNumber = appTracer.startActiveSpan(
+    "generate new coupon",
+    (span) => {
+      try {
+        return getRandomNumber(1, 25);
+      } finally {
+        span.end();
+      }
+    }
+  );
+
+  // Fetch from membership api
+  axios
+    .get(`http://${MEMBERSHIP_SERVICE_HOST}/isMember`)
     .then(function (response) {
-      console.log('Got response from membership service!', response.data);
+      console.log("Got response from membership service!", response.data);
       res.json({
-        coupon: getRandomNumber(1, 25),
-        isMember: response.data.isMember
+        coupon: newCouponNumber,
+        isMember: response.data.isMember,
       });
     })
     .catch(function (error) {
@@ -64,9 +91,9 @@ const server = app.listen(PORT, () => {
 });
 
 // Add signal handler to gracefully shutdown the server
-process.on('SIGTERM', () => {
-  console.log('Received SIGTERM. Shutting down server.');
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM. Shutting down server.");
   server.close(() => {
-    console.log('Server closed.');
+    console.log("Server closed.");
   });
 });
