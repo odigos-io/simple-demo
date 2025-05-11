58a7fa29bb824fdd88e8ff1628a01859ea785b61
Ben Elferink <ben.elferink@icloud.com>
2025-05-11 10:01:47 +0300

    fix: update currency conversion endpoints and display to use EUR instead of ILS

--- frontend/src/main/java/dev/keyval/kvshop/frontend/ProductController.java
+++ frontend/src/main/java/dev/keyval/kvshop/frontend/ProductController.java
@@ -26,11 +26,11 @@ public class ProductController {
     }
 
     @CrossOrigin(origins = "*")
-    @GetMapping("/usd-ils")
-    public int getUsdIlsConversion() {
-        int ilsConversionRate = currencyService.getConversionRate("usd-ils");
+    @GetMapping("/rate/{currencyPair}")
+    public int getConversionRate(@PathVariable String currencyPair) {
+        int conversionRate = currencyService.getConversionRate(currencyPair);
 
-        return ilsConversionRate;
+        return conversionRate;
     }
 
     @CrossOrigin(origins = "*")
@@ -54,11 +54,11 @@ public class ProductController {
     public void buyProduct(@RequestParam(name = "id") int id) {
         // Validate price via pricing service
         double price = pricingService.getPrice(id);
-        int ilsConversionRate = currencyService.getConversionRate("usd-ils");
+        int conversionRate = currencyService.getConversionRate("usd-eur");
 
         String usdPrice = ("$" + price + " USD");
-        String ilsPrice = ("ִ₪" + (price * ilsConversionRate) + " ILS");
-        System.out.println("Buying product with id " + id + " for " + usdPrice + " (converted to ִִִ" + ilsPrice + ")");
+        String eurPrice = ("ִ€" + (price * conversionRate) + " EUR");
+        System.out.println("Buying product with id " + id + " for " + usdPrice + " (converted to ִִִ" + eurPrice + ")");
 
         // Call inventory service to buy product
         this.inventoryService.buy(id);
--- frontend/webapp/components/Header.js
+++ frontend/webapp/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   const [data, setData] = useState(null)
   const [isLoading, setLoading] = useState(true)
   useEffect(() => {
-    fetch('/usd-ils')
+    fetch('/rate/usd-eur')
       .then((res) => res.json())
       .then((data) => {
         setData(data)
@@ -19,7 +19,7 @@ const Header = () => {
   return (
     <div className='bg-blue-300 min-w-screen h-50 flex justify-between items-center py-4'>
       <h1 className='px-5 text-2xl font-semibold text-gray-800'>Keyval Shop</h1>
-      <span className='px-5 font-semibold text-gray-800'>1 USD 🇺🇸 = {isLoading ? '...' : JSON.stringify(data)} ILS 🇮🇱</span>
+      <span className='px-5 font-semibold text-gray-800'>1 USD 🇺🇸 = {isLoading ? '...' : JSON.stringify(data)} EUR 🇪🇺</span>
     </div>
   )
 }
