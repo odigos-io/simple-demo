44229e965f9f2b797ceb1344e965d1cd9645d337
dan505512 <dan@odigos.io>
2026-01-04 13:10:51 +0200

    adding liveness probe to geolocation service

--- frontend/src/main/java/dev/keyval/kvshop/frontend/InventoryService.java
+++ frontend/src/main/java/dev/keyval/kvshop/frontend/InventoryService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 @Service
