25c8a81202349cd172596577dc10fbfcc8508d8a
Liran Mimony <liran@odigos.io>
2026-05-25 11:49:06 +0300

    chore: New Simple Demo apps feature: Push special HTTP header for better instrumentation rules testing

--- membership/main.go
+++ membership/main.go
@@ -15,13 +15,19 @@ import (
 )
 
 const (
-	httpPort = 8080
+	httpPort               = 8080
 	pricingEndpointEnvName = "PRICING_SERVICE_HOST"
 )
 
 type IsMemberResponse struct {
 	IsMember bool `json:"isMember"`
 }
+
+func (imr *IsMemberResponse) CheckIsMember() bool {
+	return imr.IsMember
+}
+
+var tracer = otel.Tracer("github.com/keyval-dev/test-apps/kv-shop/membership")
 
 func getPortFromEnvOrDefault() int {
 	port := httpPort
@@ -36,8 +42,6 @@ func getPortFromEnvOrDefault() int {
 	return port
 }
 
-var tracer = otel.Tracer("github.com/keyval-dev/test-apps/kv-shop/membership")
-
 func checkIfMember(ctx context.Context, userId uuid.UUID) bool {
 	_, span := tracer.Start(ctx, "checkIfMember")
 	defer span.End()
@@ -45,7 +49,10 @@ func checkIfMember(ctx context.Context, userId uuid.UUID) bool {
 	slog.Info("Checking if user is a member", "userId", userId)
 
 	// arbitrary decide if user is a member by it's id
-	isMember := userId.String()[29] >= '0' && userId.String()[29] <= '6'
+	res := IsMemberResponse{
+		IsMember: userId.String()[29] >= '0' && userId.String()[29] <= '6',
+	}
+	isMember := res.CheckIsMember()
 	span.SetAttributes(attribute.String("simple_demo.userId", userId.String()), attribute.Bool("simple_demo.is_member", isMember))
 
 	return isMember
@@ -54,8 +61,13 @@ func checkIfMember(ctx context.Context, userId uuid.UUID) bool {
 func main() {
 	port := getPortFromEnvOrDefault()
 	slog.Info("Starting Membership service", "port", port)
+
 	http.HandleFunc("/isMember", func(writer http.ResponseWriter, request *http.Request) {
 		slog.Info("isMember called")
+
+		// inject odigos-special-header
+		writer.Header().Set("odigos-special-header", "membership-service")
+		request.Header.Set("odigos-special-header", "membership-service")
 
 		userId := uuid.New()
 
@@ -77,7 +89,7 @@ func main() {
 			writer.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
+		req.Header.Set("odigos-special-header", "membership-service")
 		_, err = http.DefaultClient.Do(req)
 		if err != nil {
 			slog.Error("failed to call pricing service", "error", err)
